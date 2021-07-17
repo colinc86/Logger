fd19fe3ce8907d5e2946594d9a1ee062d4e32a7d
Colin Campbell <colinc86@gmail.com>
2021-07-16 20:03:16 -0500

    Access control.

--- Sources/Logger/Logger.swift
+++ Sources/Logger/Logger.swift
@@ -22,37 +22,37 @@ public var logger: Logger = {
 }()
 
 @inlinable
-func logTrace(_ message: @autoclosure () -> String, _ file: StaticString = #file, _ function: StaticString = #function, _ line: UInt = #line) {
+public func logTrace(_ message: @autoclosure () -> String, _ file: StaticString = #file, _ function: StaticString = #function, _ line: UInt = #line) {
   logger.trace(createMessage(from: message(), file: file, function: function, line: line))
 }
 
 @inlinable
-func logDebug(_ message: String, _ file: StaticString = #file, _ function: StaticString = #function, _ line: UInt = #line) {
+public func logDebug(_ message: String, _ file: StaticString = #file, _ function: StaticString = #function, _ line: UInt = #line) {
   logger.debug(createMessage(from: message, file: file, function: function, line: line))
 }
 
 @inlinable
-func logInfo(_ message: String, _ file: StaticString = #file, _ function: StaticString = #function, _ line: UInt = #line) {
+public func logInfo(_ message: String, _ file: StaticString = #file, _ function: StaticString = #function, _ line: UInt = #line) {
   logger.info(createMessage(from: message, file: file, function: function, line: line))
 }
 
 @inlinable
-func logNotice(_ message: String, _ file: StaticString = #file, _ function: StaticString = #function, _ line: UInt = #line) {
+public func logNotice(_ message: String, _ file: StaticString = #file, _ function: StaticString = #function, _ line: UInt = #line) {
   logger.info(createMessage(from: message, file: file, function: function, line: line))
 }
 
 @inlinable
-func logWarning(_ message: String, _ file: StaticString = #file, _ function: StaticString = #function, _ line: UInt = #line) {
+public func logWarning(_ message: String, _ file: StaticString = #file, _ function: StaticString = #function, _ line: UInt = #line) {
   logger.warning(createMessage(from: message, file: file, function: function, line: line))
 }
 
 @inlinable
-func logError(_ message: String, _ file: StaticString = #file, _ function: StaticString = #function, _ line: UInt = #line) {
+public func logError(_ message: String, _ file: StaticString = #file, _ function: StaticString = #function, _ line: UInt = #line) {
   logger.error(createMessage(from: message, file: file, function: function, line: line))
 }
 
 @inlinable
-func logCritical(_ message: String, _ file: StaticString = #file, _ function: StaticString = #function, _ line: UInt = #line) {
+public func logCritical(_ message: String, _ file: StaticString = #file, _ function: StaticString = #function, _ line: UInt = #line) {
   logger.critical(createMessage(from: message, file: file, function: function, line: line))
 }
 
@@ -64,7 +64,7 @@ func logCritical(_ message: String, _ file: StaticString = #file, _ function: St
 ///   - file: The file string.
 ///   - line: The line number.
 /// - Returns: A formatted logger message.
-public func createMessage(from message: String, file: StaticString, function: StaticString, line: UInt) -> Logger.Message {
+fileprivate func createMessage(from message: String, file: StaticString, function: StaticString, line: UInt) -> Logger.Message {
   let filename = ("\(file)".components(separatedBy: "/").last ?? "").components(separatedBy: ".").first ?? ""
   let function = "\(function)".components(separatedBy: "(").first ?? ""
   return Logger.Message("[\(filename).\(function):\(line)] \(message)")
