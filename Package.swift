af0f16381ba136ca9729b4b4b1b0d2e56351fcf7
Colin Campbell <colinc86@gmail.com>
2021-07-16 19:59:54 -0500

    Initial commit.

--- Package.swift
+++ Package.swift
@@ -4,25 +4,26 @@
 import PackageDescription
 
 let package = Package(
-    name: "Logger",
-    products: [
-        // Products define the executables and libraries a package produces, and make them visible to other packages.
-        .library(
-            name: "Logger",
-            targets: ["Logger"]),
-    ],
-    dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
-    ],
-    targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages this package depends on.
-        .target(
-            name: "Logger",
-            dependencies: []),
-        .testTarget(
-            name: "LoggerTests",
-            dependencies: ["Logger"]),
-    ]
+  name: "Logger",
+  products: [
+    // Products define the executables and libraries a package produces, and make them visible to other packages.
+    .library(
+      name: "Logger",
+      targets: ["Logger"]),
+  ],
+  dependencies: [
+    // Dependencies declare other packages that this package depends on.
+    .package(url: "https://github.com/apple/swift-log", from: "1.4.2"),
+    .package(url: "https://github.com/colinc86/ApplicationKey", from: "0.1.1"),
+  ],
+  targets: [
+    // Targets are the basic building blocks of a package. A target can define a module or a test suite.
+    // Targets can depend on other targets in this package, and on products in packages this package depends on.
+    .target(
+      name: "Logger",
+      dependencies: ["ApplicationKey", "Logging"]),
+    .testTarget(
+      name: "LoggerTests",
+      dependencies: ["Logger"]),
+  ]
 )
--- Sources/Logger/Logger.swift
+++ Sources/Logger/Logger.swift
@@ -1,6 +1,71 @@
-public struct Logger {
-    public private(set) var text = "Hello, World!"
+//
+//  Logger.swift
+//  Logging
+//
+//  Created by Colin Campbell on 4/29/21.
+//
 
-    public init() {
-    }
+import Foundation
+import Logging
+
+/// The applications main logger.
+public var logger: Logger = {
+  var log = Logger(label: ApplicationKey(from: "logger"))
+  
+  #if DEBUG
+  log.logLevel = .trace
+  #else
+  log.logLevel = .info
+  #endif
+  
+  return log
+}()
+
+@inlinable
+func logTrace(_ message: @autoclosure () -> String, _ file: StaticString = #file, _ function: StaticString = #function, _ line: UInt = #line) {
+  logger.trace(createMessage(from: message(), file: file, function: function, line: line))
+}
+
+@inlinable
+func logDebug(_ message: String, _ file: StaticString = #file, _ function: StaticString = #function, _ line: UInt = #line) {
+  logger.debug(createMessage(from: message, file: file, function: function, line: line))
+}
+
+@inlinable
+func logInfo(_ message: String, _ file: StaticString = #file, _ function: StaticString = #function, _ line: UInt = #line) {
+  logger.info(createMessage(from: message, file: file, function: function, line: line))
+}
+
+@inlinable
+func logNotice(_ message: String, _ file: StaticString = #file, _ function: StaticString = #function, _ line: UInt = #line) {
+  logger.info(createMessage(from: message, file: file, function: function, line: line))
+}
+
+@inlinable
+func logWarning(_ message: String, _ file: StaticString = #file, _ function: StaticString = #function, _ line: UInt = #line) {
+  logger.warning(createMessage(from: message, file: file, function: function, line: line))
+}
+
+@inlinable
+func logError(_ message: String, _ file: StaticString = #file, _ function: StaticString = #function, _ line: UInt = #line) {
+  logger.error(createMessage(from: message, file: file, function: function, line: line))
+}
+
+@inlinable
+func logCritical(_ message: String, _ file: StaticString = #file, _ function: StaticString = #function, _ line: UInt = #line) {
+  logger.critical(createMessage(from: message, file: file, function: function, line: line))
+}
+
+// MARK: Private methods
+
+/// Creates a log message.
+/// - Parameters:
+///   - message: The message's value.
+///   - file: The file string.
+///   - line: The line number.
+/// - Returns: A formatted logger message.
+public func createMessage(from message: String, file: StaticString, function: StaticString, line: UInt) -> Logger.Message {
+  let filename = ("\(file)".components(separatedBy: "/").last ?? "").components(separatedBy: ".").first ?? ""
+  let function = "\(function)".components(separatedBy: "(").first ?? ""
+  return Logger.Message("[\(filename).\(function):\(line)] \(message)")
 }
