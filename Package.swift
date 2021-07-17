d7591f4224dab387b0b5508c121b4eedbfcb243a
Colin Campbell <colinc86@gmail.com>
2021-07-16 20:09:39 -0500

    Update package reference yeah.

--- Package.swift
+++ Package.swift
@@ -21,7 +21,7 @@ let package = Package(
     // Targets can depend on other targets in this package, and on products in packages this package depends on.
     .target(
       name: "Logger",
-      dependencies: ["ApplicationKey", "swift-log"]),
+      dependencies: ["ApplicationKey", .product(name: "Logging", package: "swift-log")]),
     .testTarget(
       name: "LoggerTests",
       dependencies: ["Logger"]),
